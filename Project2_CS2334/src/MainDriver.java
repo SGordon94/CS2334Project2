ec781221ab8156d4e66e782628047dc2435dcd1e
stephon.p.i.gordon-1@ou.edu <Stephon@10.193.6.109>
2013-10-05 13:00:56 -0500

    lol

--- Project2_CS2334/src/MainDriver.java
+++ Project2_CS2334/src/MainDriver.java
@@ -8,7 +8,8 @@ public class MainDriver {
 		FileIO.getInput();
 		FileIO.readFile();
 		
-		//Paper testPaper1 = Paper();
+		//Paper testPaper1 = Paper("Conference Paper", "Woehrer, Mark; Hougen, Dean; Schlupp, Ingo",
+		//		"Sexual Selection, Resource Distribution, and Population Size in Synthetic Sympatric Speciation",);
 	}
 
 }
