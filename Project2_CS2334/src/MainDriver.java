dbb7964318c2d3cdbf51af79439d2bed68dcd144
Daylon Wilkins <hydrocharged-github@yahoo.com>
2013-10-07 17:16:35 -0500

    adsf

--- Project2_CS2334/src/MainDriver.java
+++ Project2_CS2334/src/MainDriver.java
@@ -143,7 +143,7 @@ public class MainDriver {
 					System.out.println("AN: sort by author names");
 					System.out.println("PT: sort by paper title");
 					System.out.println("ST: sort by serial title");
-					System.out.println("CH: sort by chronological order. Dates must be in \"Month Year\" format.");
+					System.out.println("CH: sort by chronological order; dates must be in \"MONTH YEAR\" format");
 					System.out.println("R:  randomly sort");
 					System.out.println("PS: print the papers to the screen");
 					System.out.println("PF: write the papers to a file");
