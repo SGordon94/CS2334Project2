54fe1887a4c5c48dfa0fec080ff5ead08f0e5ef8
Stephon Gordon <stephon.p.i.gordon-1@ou.edu>
2013-11-12 20:51:23 -0600

    view

---
+++ Project2_CS2334/src/SelectionView.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+public class SelectionView extends JFrame {
+	
+	JPanel mainPanel = new JPanel();
+	LayoutManager layout1 = new GridLayout(1,3, 5, 5);
+	
+	
+	JPanel scholarsPanel = new JPanel();
+	JList listOfScholars = new JList();
+	
+	
+	
+	JPanel serialsPanel = new JPanel();
+	JList listOfSerials = new JList();
+	
+	
+	JPanel papersPanel = new JPanel();
+	JList listOfPapers = new JList();
+	
+}
