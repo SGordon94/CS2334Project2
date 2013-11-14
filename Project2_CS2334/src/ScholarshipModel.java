32cb4d00ae6ae21b94888d8716b643dad6b00fd5
Daylon Wilkins <hydrocharged-github@yahoo.com>
2013-11-13 18:11:21 -0600

    af

--- Project2_CS2334/src/ScholarshipModel.java
+++ Project2_CS2334/src/ScholarshipModel.java
@@ -1,4 +1,8 @@
+import java.util.*;
+
 
 public class ScholarshipModel {
-
+	private ArrayList<Conference> conferences;
+	private ArrayList<Journal> journals;
+	private ArrayList<Scholar> scholars;
 }
---
+++ Project2_CS2334/src/Serial.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class Serial {
+	private String serialTitle;
+	private ArrayList<Scholar> scholars;
+	private ArrayList<Paper> papers;
+}
