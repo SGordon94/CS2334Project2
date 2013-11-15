aa5d9cfb2b115cfdda350debb8c87b2b8ff2487f
Stephon Gordon <stephon.p.i.gordon-1@ou.edu>
2013-11-15 02:09:25 -0600

    GUISelectionView

--- Project2_CS2334/src/SelectionView.java
+++ Project2_CS2334/src/SelectionView.java
@@ -16,23 +16,83 @@ public class SelectionView extends JFrame implements ActionListener {
 	private JButton jbtDeletePapers = new JButton("Delete Selected Papers(s)");
 	private JButton jbtDeleteAllPapers = new JButton("Delete All Papers");
 	
-	JPanel mainPanel = new JPanel();
-	LayoutManager layout1 = new GridLayout(1,3, 5, 5);
 	
-	JPanel scholarsPanel = new JPanel();
+
 	JList listOfScholars = new JList();
 	
-	JPanel serialsPanel = new JPanel();
+
 	JList listOfSerials = new JList();
 	
-	JPanel papersPanel = new JPanel();
+
 	JList listOfPapers = new JList();
 	
 	public SelectionView(){
 		setTitle("ScholarPub");
 		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new GridLayout(1,3,5,5));
+		
+		//
+		JPanel scholarsPanel = new JPanel();
+		scholarsPanel.setLayout(new BorderLayout());
+		
+		scholarsPanel.add(new JLabel("Scholars List"), BorderLayout.NORTH);
+		
+		scholarsPanel.add(new List(), BorderLayout.CENTER);
+		
+		JPanel scholarsButtonPanel = new JPanel();
+		scholarsButtonPanel.setLayout(new GridLayout(2,2,2,3));
+		scholarsButtonPanel.add(jbtAddScholar);
+		scholarsButtonPanel.add(jbtDeleteScholars);
+		scholarsButtonPanel.add(jbtDeleteAllScholars);
+		
+		scholarsPanel.add(scholarsButtonPanel, BorderLayout.SOUTH);
+		//
+		
+		//
+		JPanel serialsPanel = new JPanel();
+		serialsPanel.setLayout(new BorderLayout());
+		
+		serialsPanel.add(new JLabel("Serials List"), BorderLayout.NORTH);
+		
+		serialsPanel.add(new List(), BorderLayout.CENTER);
+		
+		JPanel serialsButtonPanel = new JPanel();
+		serialsButtonPanel.setLayout(new GridLayout(2,2,2,3));
+		serialsButtonPanel.add(jbtAddSerial);
+		serialsButtonPanel.add(jbtDeleteSerials);
+		serialsButtonPanel.add(jbtDeleteAllSerials);
+		
+		serialsPanel.add(serialsButtonPanel, BorderLayout.SOUTH);
+		//
+		
+		//
+		JPanel papersPanel = new JPanel();
+		papersPanel.setLayout(new BorderLayout());
+		
+		papersPanel.add(new JLabel("Papers List"), BorderLayout.NORTH);
+		
+		papersPanel.add(new JList(), BorderLayout.CENTER);
+		
+		JPanel papersButtonPanel = new JPanel();
+		papersButtonPanel.setLayout(new GridLayout(2,2,2,3));
+		papersButtonPanel.add(jbtAddPaper);
+		papersButtonPanel.add(jbtDeletePapers);
+		papersButtonPanel.add(jbtDeleteAllPapers);
+		
+		papersPanel.add(papersButtonPanel, BorderLayout.SOUTH);
+		
+		mainPanel.add(scholarsPanel);
+		
+		mainPanel.add(serialsPanel);
+		
+		mainPanel.add(papersPanel);
+		
+		add(mainPanel);
+		
+		setSize(1200,400);
 		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 	}
 
