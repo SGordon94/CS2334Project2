1fdcd7179d90b92daae83a3930c94b05d9e2805f
Stephon Gordon <stephon.p.i.gordon-1@ou.edu>
2013-11-19 20:50:17 -0600

    finishedaddserialgui

--- Project2_CS2334/src/AddSerialView.java
+++ Project2_CS2334/src/AddSerialView.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.border.*;
 
 public class AddSerialView extends JFrame implements ItemListener{
 	private final String CONFERENCE = "Conference";
@@ -39,7 +40,7 @@ public class AddSerialView extends JFrame implements ItemListener{
 		add(buttonPanel, BorderLayout.SOUTH);
 		
 		
-		setSize(600, 410);
+		setSize(600, 470);
 		setLocationRelativeTo(null);
 		//pack();
 		
@@ -54,30 +55,33 @@ public class AddSerialView extends JFrame implements ItemListener{
 		 */
 		private static final long serialVersionUID = -8523558338836188027L;
 		private JLabel nameLabel = new JLabel("Name: ");
-		private JTextField name = new JTextField(" Name of Conference", 30);
-		private JLabel meetingLabel = new JLabel("Meeting Details");
+		private JTextField name = new JTextField(40);
+		private JLabel organizationLabel = new JLabel("Organization: ");
+		private JTextField organization = new JTextField(36);
 		private JLabel monthLabel = new JLabel("Month: ");
-		private JTextField month = new JTextField(" Month", 7);
+		private JTextField month = new JTextField(7);
 		private JLabel yearLabel = new JLabel("Year: ");
-		private JTextField year = new JTextField(" Year", 7);
+		private JTextField year = new JTextField(7);
 		private JLabel cityLabel = new JLabel("City: ");
-		private JTextField city = new JTextField(" City", 6);
+		private JTextField city = new JTextField(7);
 		private JLabel stateProvinceLabel = new JLabel("State/Province: ");
-		private JTextField stateProvince = new JTextField(" State/Province");
+		private JTextField stateProvince = new JTextField(12);
 		private JLabel countryLabel = new JLabel("Country: ");
-		private JTextField country = new JTextField(" Country", 5);
+		private JTextField country = new JTextField(7);
 		
 		private JLabel programChairsLabel = new JLabel("Program Chairs");
 		private JList  programChairs = new JList();
 		private JLabel programCommitteeMembersLabel = new JLabel("Committe Members");
 		private JList  programCommitteeMembers = new JList();
 		
-		private JLabel publishedPapersLabel = new JLabel("Published Papers");
-		private JList  publishedPapers = new JList();
+		private String meetingLabel = "Meeting Details";
+		private Border etchedBorder = BorderFactory.createEtchedBorder(EtchedBorder.LOWERED);
+		private TitledBorder titledBorder = BorderFactory.createTitledBorder(etchedBorder, meetingLabel,
+				TitledBorder.LEFT, TitledBorder.ABOVE_TOP);
 		
 		public ConferenceView(){
 			JPanel mainPanel1 = new JPanel();
-			mainPanel1.setLayout(new GridLayout(3,1,0,0));
+			mainPanel1.setLayout(new GridLayout(2,1,0,0));
 			
 			JPanel topPanel = new JPanel();
 			topPanel.setLayout(new GridLayout(4,1,0,0));
@@ -85,6 +89,10 @@ public class AddSerialView extends JFrame implements ItemListener{
 			JPanel namePanel = new JPanel();
 			namePanel.add(nameLabel);
 			namePanel.add(name);
+			
+			JPanel organizationPanel = new JPanel();
+			organizationPanel.add(organizationLabel);
+			organizationPanel.add(organization);
 			
 			JPanel monthPanel = new JPanel();
 			monthPanel.add(monthLabel);
@@ -101,19 +109,18 @@ public class AddSerialView extends JFrame implements ItemListener{
 			cityPanel.add(country);
 			
 			
-			topPanel.add(meetingLabel);
+			
 			topPanel.add(namePanel);
+			topPanel.add(organizationPanel);
 			topPanel.add(monthPanel);
 			topPanel.add(cityPanel);
+			topPanel.setBorder(titledBorder);
 			
-			//middle panel
 			
-			JPanel middlePanel = new JPanel();
-			middlePanel.setLayout(new GridLayout(1,2,4,4));
-//			middlePanel.add(programCommitteeMembersLabel);
-//			middlePanel.add(programChairsLabel);
-//			middlePanel.add(programCommitteeMembers);
-//			middlePanel.add(programChairs);
+			//bottom panel
+			
+			JPanel bottomPanel = new JPanel();
+			bottomPanel.setLayout(new GridLayout(1,2,4,4));
 			
 			JPanel leftPanel = new JPanel();
 			leftPanel.setLayout(new BorderLayout());
@@ -123,18 +130,11 @@ public class AddSerialView extends JFrame implements ItemListener{
 			rightPanel.setLayout(new BorderLayout());
 			rightPanel.add(programCommitteeMembersLabel, BorderLayout.NORTH);
 			rightPanel.add(new JScrollPane(programCommitteeMembers));
-			middlePanel.add(leftPanel);
-			middlePanel.add(rightPanel);
-			
-			//lower panel
-			JPanel lowerPanel = new JPanel();
-			lowerPanel.setLayout(new GridLayout(2,1,0,0));
-			lowerPanel.add(publishedPapersLabel);
-			lowerPanel.add(new JScrollPane(publishedPapers));
+			bottomPanel.add(leftPanel);
+			bottomPanel.add(rightPanel);
 			
 			mainPanel1.add(topPanel);
-			mainPanel1.add(middlePanel);
-			//mainPanel1.add(lowerPanel);
+			mainPanel1.add(bottomPanel);
 			
 			add(mainPanel1);
 		}
@@ -145,17 +145,99 @@ public class AddSerialView extends JFrame implements ItemListener{
 		 * 
 		 */
 		private static final long serialVersionUID = 6311171508567796171L;
+		
+		private JLabel     journalNameLabel = new JLabel("Journal: ");
+		private JTextField journalName = new JTextField(42);
+		private JLabel	   organizationLabel = new JLabel("Organization: ");
+		private JTextField organiztionName = new JTextField(39);
+		private JLabel     issueLabel = new JLabel("Issue: ");
+		private JTextField issueName = new JTextField(43);
+		private JLabel     monthLabel = new JLabel("Month: ");
+		private JTextField month = new JTextField(7);
+		private JLabel     yearLabel = new JLabel("Year: ");
+		private JTextField year = new JTextField(7);
+		private JLabel     cityLabel = new JLabel("City: ");
+		private JTextField city = new JTextField(7);
+		private JLabel     stateProvinceLabel = new JLabel("State/Province: ");
+		private JTextField stateProvince = new JTextField(12);
+		private JLabel     countryLabel = new JLabel("Country: ");
+		private JTextField country = new JTextField(7);
+		   
+		private JLabel editorsLabel = new JLabel("Editors");
+		private JList  editors = new JList();
+		private JLabel reviewersLabel = new JLabel("Reviewers");
+		private JList  reviewers = new JList();
+		
+		private String journalLabel = "Journal Details";
+		private Border etchedBorder = BorderFactory.createEtchedBorder(EtchedBorder.LOWERED);
+		private TitledBorder titledBorder = BorderFactory.createTitledBorder(etchedBorder, journalLabel,
+				TitledBorder.LEFT, TitledBorder.ABOVE_TOP);
+		
 		public JournalView(){
-			JPanel panel = new JPanel();
-			JButton jbt = new JButton("Journal Stuff");
-			panel.add(jbt);
-			add(panel);
+			JPanel mainPanel = new JPanel();
+			mainPanel.setLayout(new GridLayout(2,1,0,0));
+			
+			//top panel
+			JPanel topPanel = new JPanel();
+			topPanel.setLayout(new GridLayout(5,1,0,0));
+			
+			JPanel journalPanel = new JPanel();
+			journalPanel.add(journalNameLabel);
+			journalPanel.add(journalName);
+			
+			JPanel organizationPanel = new JPanel();
+			organizationPanel.add(organizationLabel);
+			organizationPanel.add(organiztionName);
+			
+			JPanel issuePanel = new JPanel();
+			issuePanel.add(issueLabel);
+			issuePanel.add(issueName);
+			
+			JPanel monthPanel = new JPanel();
+			monthPanel.add(monthLabel);
+			monthPanel.add(month);
+			monthPanel.add(yearLabel);
+			monthPanel.add(year);
+			
+			JPanel cityPanel = new JPanel();
+			cityPanel.add(cityLabel);
+			cityPanel.add(city);
+			cityPanel.add(stateProvinceLabel);
+			cityPanel.add(stateProvince);
+			cityPanel.add(countryLabel);
+			cityPanel.add(country);
+			
+			topPanel.add(journalPanel);
+			topPanel.add(organizationPanel);
+			topPanel.add(issuePanel);
+			topPanel.add(monthPanel);
+			topPanel.add(cityPanel);
+			topPanel.setBorder(titledBorder);
+			
+			//bottom Panel
+			JPanel bottomPanel = new JPanel();
+			bottomPanel.setLayout(new GridLayout(1,2,4,4));
+			
+			JPanel leftPanel = new JPanel();
+			leftPanel.setLayout(new BorderLayout());
+			leftPanel.add(editorsLabel, BorderLayout.NORTH);
+			leftPanel.add(new JScrollPane(editors));
+			JPanel rightPanel = new JPanel();
+			rightPanel.setLayout(new BorderLayout());
+			rightPanel.add(reviewersLabel, BorderLayout.NORTH);
+			rightPanel.add(new JScrollPane(reviewers));
+			bottomPanel.add(leftPanel);
+			bottomPanel.add(rightPanel);
+			
+			mainPanel.add(topPanel);
+			mainPanel.add(bottomPanel);
+			
+			add(mainPanel);
 		}
 	}
 	
 	@Override
 	public void itemStateChanged(ItemEvent e) {
-		// TODO Auto-generated method stub
 		CardLayout cardPanels = (CardLayout)(cards.getLayout());
 		cardPanels.show(cards, (String)(e.getItem()));
 	}
