5c32fd6664b4a2a191e9574ab36714f699e53427
Stephon Gordon <stephon.p.i.gordon-1@ou.edu>
2013-11-19 22:33:57 -0600

    paperview

--- Project2_CS2334/src/AddPaperView.java
+++ Project2_CS2334/src/AddPaperView.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+
 import java.awt.*;
 
 public class AddPaperView extends JFrame{
@@ -18,4 +19,69 @@ public class AddPaperView extends JFrame{
 	private JList scholarList = new JList();
 	private JLabel serialsListLabel = new JLabel("List of Serials");
 	private JList serialsList = new JList();
+	private JButton jbtAddPaper = new JButton("Add Paper");
+	
+	public AddPaperView(){
+		
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new BorderLayout());
+		
+		JPanel topPanel = new JPanel();
+		topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.Y_AXIS));
+		
+		JPanel paperTypePanel = new JPanel();
+		paperTypePanel.add(paperTypeLabel);
+		paperTypePanel.add(paperType);
+		
+		JPanel paperNamePanel = new JPanel();
+		paperNamePanel.add(paperNameLabel);
+		paperNamePanel.add(paperName);
+		
+		JPanel numbersPanel = new JPanel();
+		numbersPanel.add(conferencePagesLabel);
+		numbersPanel.add(conferencePages);
+		numbersPanel.add(journalNumbersLabel);
+		numbersPanel.add(journalNumbers);
+		
+		JPanel publicationDatePanel = new JPanel();
+		publicationDatePanel.add(publicationMonthLabel);
+		publicationDatePanel.add(publicationMonth);
+		publicationDatePanel.add(publicationYearLabel);
+		publicationDatePanel.add(publicationYear);
+		
+		topPanel.add(paperTypePanel);
+		topPanel.add(paperNamePanel);
+		topPanel.add(numbersPanel);
+		topPanel.add(publicationDatePanel);
+		
+		//bottom panel
+		JPanel bottomPanel = new JPanel();
+		JPanel leftPanel = new JPanel();
+		leftPanel.setLayout(new BorderLayout());
+		leftPanel.add(scholarsListLabel, BorderLayout.NORTH);
+		leftPanel.add(new JScrollPane(scholarList));
+		JPanel rightPanel = new JPanel();
+		rightPanel.add(serialsListLabel, BorderLayout.NORTH);
+		rightPanel.add(new JScrollPane(serialsList));
+		bottomPanel.add(leftPanel);
+		bottomPanel.add(rightPanel);
+		
+		//button panel
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.add(jbtAddPaper);
+		
+		mainPanel.add(topPanel, BorderLayout.NORTH);
+		mainPanel.add(bottomPanel);
+		mainPanel.add(buttonPanel);
+		
+		add(mainPanel);
+		
+		setSize(600, 500);
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+	
+	public JButton getJBTAddPaper(){
+		return jbtAddPaper;
+	}
 }
